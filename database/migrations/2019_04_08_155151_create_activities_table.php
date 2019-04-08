4f2013fd81468b446aa8c0613f02dec4d1798235
edigarhdev <edigarhdev@gmail.com>
2019-04-08 16:48:39 -0300

    Add Models and migrations

---
+++ database/migrations/2019_04_08_155151_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('module_id')->unsigned();
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
